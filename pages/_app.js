b07b8735b7abf8bc36e6f591b9c8578afcc24241
glitch <94465240+glitch-afk@users.noreply.github.com>
2022-12-10 12:14:26 +0000

    🎉 docs updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import 'nextra-theme-docs/style.css'
 import '../font.css'
 
 export default function Nextra({ Component, pageProps }) {
---
+++ theme.config.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+    logo: <img src="/logo.svg" />,
+    head: (
+        <>
+            <meta name="msapplication-TileColor" content="#ffffff" />
+            <meta name="theme-color" content="#ffffff" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta httpEquiv="Content-Language" content="en" />
+            <meta name="apple-mobile-web-app-title" content="Fetcch" />
+            <meta
+                name="description"
+                content="Fetcch Docs: Build the future of world with us"
+            />
+            <meta
+                name="og:description"
+                content="Fetcch Docs: Build the future of world with us"
+            />
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:image" content="https://docs.fetcch.xyz/og.png" />
+            <meta name="twitter:site:domain" content="docs.fetcch.xyz" />
+            <meta name="twitter:url" content="https://twitter.com/FetcchX" />
+            <meta
+                name="og:title"
+                content="Fetcch Docs: Build the future of world with us"
+            />
+            <meta name="og:image" content="https://docs.fetcch.xyz/og.png" />
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    ),
+    darkMode: true,
+    primaryHue: 33,
+    project: {
+        link: 'https://github.com/shuding/nextra-docs-template',
+    },
+    docsRepositoryBase: 'https://github.com/glitch-afk/fetcch-docs',
+    footer: {
+        text: <>{new Date().getFullYear()} © Fetcch.</>,
+    },
+}
+
+export default config
